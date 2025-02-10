f579de096d9b3903c02b443121a28a4ebc0e2bdc
Brayan Paucar <66649036+kralion@users.noreply.github.com>
2025-02-10 08:02:56 -0500

    Update _layout.web.tsx

--- app/(auth)/(tabs)/_layout.web.tsx
+++ app/(auth)/(tabs)/_layout.web.tsx
@@ -120,6 +120,11 @@ export default function WebLayout() {
   };
 
   return (
+      <View className="flex-1 "> <Stack
+          screenOptions={{
+            headerShown: false,
+          }}
+        />
     <View className="flex-row left-0 right-0 bg-white dark:bg-zinc-800 justify-center relative">
       {!isMobile && (
         <View
@@ -163,13 +168,8 @@ export default function WebLayout() {
         </View>
       )}
 
-      <View className="flex-1 w-full max-w-[611px] bg-transparent">
-        <Stack
-          screenOptions={{
-            headerShown: false,
-          }}
-        />
-      </View>
+       
+    
       {isMobile && (
         <View
           className={`fixed bottom-0 left-0 right-0 h-16 flex-row border-t ${
@@ -214,6 +214,6 @@ export default function WebLayout() {
           ))}
         </View>
       )}
-    </View>
+    </View>  </View>
   );
 }
