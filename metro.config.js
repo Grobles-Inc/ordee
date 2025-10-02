0c338ec03c60fc91fb640785f02673277f39621f
kralion <joan300501@gmail.com>
2025-10-02 12:38:35 -0500

    some modifications

--- metro.config.js
+++ metro.config.js
@@ -1,11 +1,5 @@
 const { getDefaultConfig } = require("expo/metro-config");
 const { withNativeWind } = require("nativewind/metro");
-const {
-  wrapWithReanimatedMetroConfig,
-} = require("react-native-reanimated/metro-config");
 const config = getDefaultConfig(__dirname);
-config.resolver.unstable_enablePackageExports = false;
 
-module.exports = withNativeWind(wrapWithReanimatedMetroConfig(config), {
-  input: "./styles/global.css",
-});
+module.exports = withNativeWind(config, { input: './styles/global.css', inlineRem: 16 });
